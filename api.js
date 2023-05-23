bc2c4e32c28a5680c751be70c64b479886b6478f
Brian Lynch <brlynch@ptc.com>
2023-05-23 09:55:33 -0400

    Revision: Cleared value in memory after process is complete.

--- api.js
+++ api.js
@@ -116,10 +116,12 @@ apiRouter.get('/gltf/:tid', async (req, res) => {
             } else {
                 forwardRequestToOnshape(`${onshapeApiUrl}/documents/d/${transJson.documentId}/externaldata/${transJson.resultExternalDataIds[0]}`, req, res);
             }
+
             const webhookID = results;
             WebhookService.unregisterWebhook(webhookID, req.user.accessToken)
                 .then(() => console.log(`Webhook ${webhookID} unregistered successfully`))
                 .catch((err) => console.error(`Failed to unregister webhook ${webhookID}: ${JSON.stringify(err)}`));
+            delete inMemoryDataStore[req.params.tid];
         }
     }
 });
