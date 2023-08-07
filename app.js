6479ef3105a33bb552c0321b3ae0ecff86dc5708
pbhide17 <pbhide@ptc.com>
2023-08-07 14:27:18 -0400

    Removed debug log from oauthredirect path

--- app.js
+++ app.js
@@ -61,10 +61,7 @@ app.use('/oauthSignin', (req, res) => {
     return passport.authenticate('onshape', { state: uuid.v4(state) })(req, res);
 }, (req, res) => {     console.log("Redirecting to Onshape for authentication");/* redirected to Onshape for authentication */ });
 
-app.use('/oauthRedirect', (req, res) => {
-    console.log("In /oauthRedirect middleware");
-    return passport.authenticate('onshape', { failureRedirect: '/grantDenied' })(req, res);
-}, (req, res) => {
+app.use('/oauthRedirect', passport.authenticate('onshape', { failureRedirect: '/grantDenied' }), (req, res) => {
     res.redirect(`/?documentId=${req.session.state.docId}&workspaceId=${req.session.state.workId}&elementId=${req.session.state.elId}`);
 });
 
